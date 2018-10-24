1e378838796eaa3d75af846f8e3bfe86c1641fcc
Jonathan Luk <jluk87@gmail.com>
2018-10-24 20:00:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-
       erb :index
     end
 
     post '/team' do
-      @team = Team.new(params)
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       erb :team
     end
 
