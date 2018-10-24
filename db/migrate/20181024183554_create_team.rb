5e9c29243616ab745479de09da519a6f40976a8a
Jonathan Luk <jluk87@gmail.com>
2018-10-24 19:02:15 +0000

    automatically backed up by learn

--- db/migrate/20181024183554_create_team.rb
+++
@@ -1,9 +0,0 @@
-class CreateTeam < ActiveRecord::Migration[5.2]
-  def change
-    create_table :teams do |t|
-      t.string :name
-      t.string :motto
-      t.string :members
-    end
-  end
-end
--- db/migrate/20181024185801_rename_table_to_heroes.rb
+++
@@ -1,5 +0,0 @@
-class RenameTableToHeroes < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :heros, :heroes
-  end
-end
