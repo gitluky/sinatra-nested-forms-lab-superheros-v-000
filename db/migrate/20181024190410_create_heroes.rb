120c5df23fcc2adf893fbf86b459f8f257051130
Jonathan Luk <jluk87@gmail.com>
2018-10-24 19:05:34 +0000

    automatically backed up by learn

--- db/migrate/20181024190410_create_heroes.rb
+++ db/migrate/20181024190410_create_heroes.rb
@@ -1,4 +1,9 @@
 class CreateHeroes < ActiveRecord::Migration[5.2]
   def change
+    create_table :heroes do |t|
+      t.string :name
+      t.string :power
+      t.string :bio
+      t.integer :team_id
   end
 end
