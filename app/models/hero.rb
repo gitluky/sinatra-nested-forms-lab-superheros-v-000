b3a172acfabd7262b5cd5668a459c970a5c1a90a
Jonathan Luk <jluk87@gmail.com>
2018-10-24 18:47:04 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,16 +1,25 @@
-class Hero < ActiveRecord::Base
-
+class Hero
   belongs_to :team
 
-  def initialize(args)
-    @name = args[:name]
-    @power = args[:power]
-    @bio = args[:bio]
-    @@all << self
-  end
-
-  def self.all
-    @all
-  end
 
 end
+
+
+
+# class Hero
+#
+#   attr_accessor :name, :power, :bio
+#   @@all = []
+#
+#   def initialize(args)
+#     @name = args[:name]
+#     @power = args[:power]
+#     @bio = args[:bio]
+#     @@all << self
+#   end
+#
+#   def self.all
+#     @all
+#   end
+#
+# end
