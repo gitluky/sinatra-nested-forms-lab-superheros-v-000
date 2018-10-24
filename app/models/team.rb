dec8445ab2b34d99e485ef293ced1527ea883fc8
Jonathan Luk <jluk87@gmail.com>
2018-10-24 18:46:38 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,17 +1,23 @@
-class Team
-
-  attr_accessor :name, :motto, :members
-
-  def initialize(params)
-    @name = params[:team][:name]
-    @motto = params[:team][:motto]
-    @members =[]
-    if !params[:team][:heroes].empty?
-      params[:team][:heroes].each do |hero|
-        new_hero = Hero.new(hero)
-        @members << new_hero
-      end
-    end
-  end
+class Team < ActiveRecord::Base
+  has_many :heroes
 
 end
+
+
+# class Team
+#
+#   attr_accessor :name, :motto, :members
+#
+#   def initialize(params)
+#     @name = params[:team][:name]
+#     @motto = params[:team][:motto]
+#     @members =[]
+#     if !params[:team][:heroes].empty?
+#       params[:team][:heroes].each do |hero|
+#         new_hero = Hero.new(hero)
+#         @members << new_hero
+#       end
+#     end
+#   end
+#
+# end
