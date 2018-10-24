e500b9f4c6e78bdd0ad9921f8bdb46f87d4165b3
Jonathan Luk <jluk87@gmail.com>
2018-10-24 19:18:28 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,6 @@ connection = ActiveRecord::Base.establish_connection(
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
-require_relative './inflections/inflections.rb'
+require_relative './initializers/inflections.rb'
 
 require_all 'app'
